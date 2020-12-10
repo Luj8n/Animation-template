cf967a947a5f0429adf032d161e00568bacc6f0f
Luj8n <l.milieska@gmail.com>
2020-12-10 17:16:53 +0200

    scaling, rotating the image

--- index.js
+++ index.js
@@ -8,8 +8,17 @@ function setup() {
   L.createCanvas(800, 800);
 }
 
+let iteration = 1;
+
 function draw() {
   L.centerCanvas();
-  L.background("black");
-  L.Image(image, 0, 0, L.width, L.height);
+  L.background("#111111");
+  L.translate(400, 400);
+  let scale = L.map(Math.sin(L.degToRad(iteration)), -1, 1, 0.3, 1);
+  L.scale(scale);
+  L.rotate(L.degToRad(1));
+  L.Image(image, -400, -400, L.width, L.height);
+  L.resetScale();
+  L.resetTranslation();
+  iteration++;
 }
